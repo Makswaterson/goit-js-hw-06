11734d33bf35a3f70ab391eb1aba4fa0d037114c
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-11-07 01:48:37 +0200

    add task 8

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,17 @@
+const btnSubmitRef = document.querySelector('.login-form');
 
+const onBtnSubmit = event => {
+  event.preventDefault();
+  const { email, password } = event.currentTarget;
+  if (email.value === '' || password.value === '') {
+    alert('Заповність будь ласка всі поля');
+  }
+  const userData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(userData);
+  btnSubmitRef.reset();
+};
+
+btnSubmitRef.addEventListener('submit', onBtnSubmit);
