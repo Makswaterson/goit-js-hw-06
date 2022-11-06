aec252ee36ae1842d06f3e2730d24c2dd35f7aa9
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-11-06 13:23:44 +0200

    update task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const liIdRef = document.querySelector('#ingredients');
+
+const listEl = ingredients.map(ingredient => {
+  const liCreateEl = document.createElement('li');
+  liCreateEl.textContent = `${ingredient}`;
+  liCreateEl.classList.add('item');
+  return liCreateEl;
+});
+
+liIdRef.append(...listEl);
+console.log(listEl);
