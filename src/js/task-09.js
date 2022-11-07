d9c49fe6438490c5d39bc7a4abd3f8aaf9a53f2e
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-11-07 17:02:58 +0200

    add task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyColorRef = document.querySelector('body');
+const btnChangeColorRef = document.querySelector('.change-color');
+const colorSpanRef = document.querySelector('.color');
+
+const onBgColorChange = event => {
+  const currentColor = getRandomHexColor();
+  colorSpanRef.textContent = `${currentColor}`;
+  bodyColorRef.style.backgroundColor = currentColor;
+};
+
+btnChangeColorRef.addEventListener('click', onBgColorChange);
