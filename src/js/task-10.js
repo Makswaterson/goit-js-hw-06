6c7ce81a915d40e9f8e6fbece061b54a9e2598df
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-11-07 20:40:06 +0200

    add task 10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,3 +1,34 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const inputBtnRef = document.querySelector('input');
+const createBtnRef = document.querySelector('[data-create]');
+const destroyBtnRef = document.querySelector('[data-destroy]');
+const boxesRef = document.querySelector('#boxes');
+
+createBtnRef.addEventListener('click', onCreateBox);
+destroyBtnRef.addEventListener('click', onDestroyBox);
+inputBtnRef.addEventListener('input', onInputNumber);
+
+let number = 0;
+function onInputNumber() {
+  number = Number(event.target.value);
+  console.log(number);
+}
+const markup = [];
+
+function onCreateBox() {
+  for (let i = 0; i < number; i++) {
+    const markupBox = `<div style="width: ${30 + i * 10}px; heigth: ${30 + i * 10}px;
+     background-color: ${getRandomHexColor()}"></div>`;
+    boxesRef.insertAdjacentHTML('afterbegin', markupBox);
+    console.log(markupBox);
+  }
+}
+console.log(boxesRef);
+
+function onDestroyBox() {
+  boxesRef.innerHTML = '';
+  inputBtnRef.value = '';
+}
