5f7bd1ebae271557b1bd41a5098d5436b6981280
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-11-21 15:44:09 +0200

    fix mentor

--- js/task-08.js
+++ js/task-08.js
@@ -3,12 +3,12 @@ const btnSubmitRef = document.querySelector('.login-form');
 const onBtnSubmit = event => {
   event.preventDefault();
   const { email, password } = event.currentTarget;
-  if (email.value === '' || password.value === '') {
+  if (email.value.trim() === '' || password.value.trim() === '') {
     return alert('Заповність будь ласка всі поля');
   }
   const userData = {
-    email: email.value,
-    password: password.value,
+    email: email.value.trim(),
+    password: password.value.trim(),
   };
   console.log(userData);
   btnSubmitRef.reset();
